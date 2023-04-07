d4441d028739f0791fbd5ad82ff0ed0d387ba73f
donerre <r.sorianotorrejon@student.han.nl>
2023-04-07 19:44:38 +0200

    start project

--- src/main/java/han/bp/dolhof/Main.java
+++
@@ -1,7 +0,0 @@
-package han.bp.dolhof;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/han/bp/dolhof/dolhofMainApp.java
@@ -0,0 +1,19 @@
+package han.bp.dolhof;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+public class dolhofMainApp extends YaegerGame {
+    public static void main(String[] args) {
+        launch(args );
+    }
+
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void setupScenes() {
+
+    }
+}
